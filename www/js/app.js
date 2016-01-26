e9a62d26e7a9ee7679c7f5be8ab2b2cb15f84f3a
ggarg2 <gauravgarg@wedealnow.com>
2016-01-26 14:51:28 +0530

    About Us Page Changes done. Contact Us Page Changes done. Rate Card Page Changes done.

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
   // angular.module is a global place for creating, registering and retrieving Angular modules
   // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
   // the 2nd parameter is an array of 'requires'
-  angular.module('app.projectX', ['ionic', 'ui.router','ngAnimate'])
+  angular.module('app.projectX', ['ionic', 'ui.router','ngAnimate', 'ion-autocomplete'])
 
   .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
@@ -66,11 +66,8 @@
             'mainContent': {
                 templateUrl: "templates/booking.html",
                 controller: 'bookingCtrl'
-            },
-
-            'fabContent': {
-                template: '<h1> Booking Footer</h1>',
             }
+
         }
    })
 
--- www/js/constant.js
+++ www/js/constant.js
@@ -1,14 +1,18 @@
 angular.module('app.projectX').constant('menuConstant', {
 	menuList:[{
+		imagepath:"img/icons/wallet-travel.png",
 		name:"Booking", 
 		state: "main.booking"
 	},{
-		name:"Pricing", 
+		imagepath:"img/icons/currency-inr.png",
+		name:"Rate Card", 
 		state: "main.pricing"
 	},{
+		imagepath:"img/icons/phone-in-talk.png",
 		name:"ContactUs", 
 		state: "main.contactUs"
 	},{
+		imagepath:"img/icons/alert-circle.png",
 		name:"About Us", 
 		state: "main.aboutUs"
 	}]
