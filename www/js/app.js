8a65b9a54e146ede9e6801346b994c790ea971a8
Gaurav Garg <garg.gaurav.1989it@gmail.com>
2016-03-15 15:30:13 +0530

    finally app is working in mobile, emulator

--- www/js/app.js
+++ www/js/app.js
@@ -6,21 +6,35 @@
   angular.module('app.projectX', ['ionic', 'ui.router','ngAnimate', 'angular-jwt', 'ion-autocomplete',
   'angular-storage', 'ngResource','ionMdInput', 'app.env.config' , 'formlyIonic','ngCordova'])
 
-      .run(function($ionicPlatform) {
+      .run(function($ionicPlatform, $ionicPopup) {
         $ionicPlatform.ready(function() {
           if(window.cordova && window.cordova.plugins.Keyboard) {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+              // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+              // for form inputs)
+              cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-        // Don't remove this line unless you know what you are doing. It stops the viewport
-        // from snapping when text inputs are focused. Ionic handles this internally for
-        // a much nicer keyboard experience.
-        cordova.plugins.Keyboard.disableScroll(true);
-      }
-      if(window.StatusBar) {
-        StatusBar.styleDefault();
-      }
+              // Don't remove this line unless you know what you are doing. It stops the viewport
+              // from snapping when text inputs are focused. Ionic handles this internally for
+              // a much nicer keyboard experience.
+              cordova.plugins.Keyboard.disableScroll(true);
+            }
+          if(window.StatusBar) {
+            StatusBar.styleDefault();
+          }
+
+          if(window.Connection) {
+              if(navigator.connection.type == Connection.NONE) {
+                  $ionicPopup.confirm({
+                      title: "Internet Disconnected",
+                      content: "The internet is disconnected on your device."
+                  })
+                  .then(function(result) {
+                      if(!result) {
+                          ionic.Platform.exitApp();
+                      }
+                  });
+              }
+          }
     });
   })
 
