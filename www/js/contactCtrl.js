d6b786ac275c831a2f52a918a4527c9bcf60c020
Gaurav Garg <garg.gaurav.1989it@gmail.com>
2016-04-14 00:01:08 +0530

    introduced contact us

--- www/js/contactCtrl.js
+++ www/js/contactCtrl.js
@@ -1,9 +1,28 @@
-angular.module('app.projectX').controller('contactCtrl', [ '$timeout','ionicMaterialMotion', 'ionicMaterialInk',
-	 function($timeout, ionicMaterialMotion, ionicMaterialInk) {
+angular.module('app.projectX').controller('contactCtrl', [ '$timeout','ionicMaterialMotion', 
+	'ionicMaterialInk', '$scope', '$rootScope', 'projectApi',
+	 function($timeout, ionicMaterialMotion, ionicMaterialInk, $scope, $rootScope, projectApi) {
+
+	$scope.refData = {};
+
+	$scope.getAllContacts = function(id, exp){
+	    projectApi.getResource('getContactUs', id, exp).then(function(response){
+	        console.log("Suuccess Handler");
+	        $scope.refData.contacts = response.data;
+	      }, function(error){
+	        console.log("Failure Handler");
+	         if(error.data !=undefined && error.data.errorMsg !=undefined){
+	             $rootScope.showAlert('Please try again' , error.data.errorMsg);
+	          }else{
+	            //showAlertBox('Please try again' , error.data);
+	          }
+	      })
+  		}
+
+  $scope.getAllContacts();
 		
-    	$timeout(function () {
-            ionicMaterialMotion.ripple();
-            ionicMaterialInk.displayEffect();
-        });
+	$timeout(function () {
+	    ionicMaterialMotion.ripple();
+	    ionicMaterialInk.displayEffect();
+	});
 
 }]);
